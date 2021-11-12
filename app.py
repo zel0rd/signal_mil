7d646353dc1a4128d8f303ec54ccb54beae4d66b
younggil <zelord.kwoun@gmail.com>
2021-11-12 10:57:42 +0900

    [FIX] index, admin_index for flask

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def index():
     return render_template('index.html')
 
 
+@app.route("/index2")
+def index2():
+    return render_template('index2.html')
+
+
 @app.route("/index_admin")
 def index_admin():
     return render_template('index_admin.html')
